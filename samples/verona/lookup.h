70f0237090e7bcb07eddd3f1f345c1a432dc5d81
Sylvan Clebsch <sylvan.clebsch@microsoft.com>
2022-10-16 02:05:57 +0100

    missing header file

---
+++ samples/verona/lookup.h
@@ -0,0 +1,57 @@
+// Copyright Microsoft and Project Verona Contributors.
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <ast.h>
+
+namespace verona
+{
+  using namespace trieste;
+
+  struct Lookup
+  {
+    // The bindings are for the context of `def`. They don't include any type
+    // arguments on `def` itself.
+    Node def;
+    Node ta;
+    NodeMap<Node> bindings;
+  };
+
+  struct Lookups
+  {
+    std::vector<Lookup> defs;
+
+    Lookups() = default;
+
+    Lookups(Node def, Node ta)
+    {
+      if (def)
+        defs.push_back({def, ta, {}});
+    }
+
+    Lookups(Lookup&& lookup)
+    {
+      if (lookup.def)
+        defs.push_back(lookup);
+    }
+
+    bool empty() const
+    {
+      return defs.empty();
+    }
+
+    void add(Lookups&& other)
+    {
+      defs.insert(defs.end(), other.defs.begin(), other.defs.end());
+    }
+
+    bool one(const std::initializer_list<Token>& types) const
+    {
+      return (defs.size()) == 1 && defs.front().def->type().in(types);
+    }
+  };
+
+  Lookups lookup_name(Node id, Node ta);
+  Lookups lookup_scopedname(Node tn);
+  Lookups lookup_scopedname_name(Node tn, Node id, Node ta);
+}
