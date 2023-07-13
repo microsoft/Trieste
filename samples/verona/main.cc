7863c190c1a2ef8f73ea35950fea4d5a85fe15b2
Sylvan Clebsch <sylvan.clebsch@microsoft.com>
2023-07-13 14:29:53 +0100

    Move the Verona compiler to microsoft/verona

--- samples/verona/main.cc
+++
@@ -1,8 +0,0 @@
-// Copyright Microsoft and Project Verona Contributors.
-// SPDX-License-Identifier: MIT
-#include "lang.h"
-
-int main(int argc, char** argv)
-{
-  return verona::driver().run(argc, argv);
-}
