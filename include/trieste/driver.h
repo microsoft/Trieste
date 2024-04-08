af58c4ac1805d98daa94152ed2bf0f12e041edb8
Matthew Parkinson <mattpark@microsoft.com>
2024-04-08 14:59:49 +0100

    Return 1 for failure, not -1.

--- include/trieste/driver.h
+++ include/trieste/driver.h
@@ -164,7 +164,7 @@ namespace trieste
         {
           logging::Error() << "Could not open " << output << " for writing."
                            << std::endl;
-          return -1;
+          return 1;
         }
       }
       else if (*test)
@@ -240,7 +240,7 @@ namespace trieste
               err << "============" << std::endl
                   << "Failed pass: " << pass->name() << ", seed: " << seed
                   << std::endl;
-              ret = -1;
+              ret = 1;
 
               if (test_failfast)
                 return ret;
